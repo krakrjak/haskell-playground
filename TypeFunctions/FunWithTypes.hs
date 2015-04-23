45bcfd74c8b77c78264ac4dcfb6013d6a2780bef
Zac Slade <Zac Slade krakrjak@gmail.com>
2015-04-23 02:35:16 -0500

    adding some adaptations and experiments from fun with type functions

---
+++ TypeFunctions/FunWithTypes.hs
@@ -0,0 +1,54 @@
+-- Needed for type family functions
+{-#LANGUAGE TypeFamilies #-}
+{-#LANGUAGE MultiParamTypeClasses #-}
+
+-- Needed for the Nat example
+{-#LANGUAGE ScopedTypeVariables #-}
+
+module FunWithTypes() where
+
+class Cons a b where
+    type ResTy a b
+    cons :: a-> [b] -> [ResTy a b]
+
+instance Cons Integer Double where
+    type ResTy Integer Double = Double
+    cons x ys = fromIntegral x : ys
+
+data Zero
+data Succ n
+
+type One   = Succ Zero
+type Two   = Succ One
+type Three = Succ Two
+type Four  = Succ Three
+type Six   = Succ (Succ Four)
+type Eight = Succ (Succ Six)
+
+class Nat n where
+    toInt :: n -> Int
+
+instance Nat Zero where
+    toInt _ = 0
+
+instance (Nat n) => Nat (Succ n) where
+    toInt _ = 1 + toInt (undefined :: n)
+
+type family GCD d m n
+type instance GCD d Zero Zero            = d
+type instance GCD d (Succ m) (Succ n)    = GCD (Succ d) m n
+type instance GCD Zero (Succ m) Zero     = Succ m
+type instance GCD Zero Zero (Succ n)     = Succ n
+type instance GCD (Succ d) (Succ m) Zero = GCD (Succ Zero) d m
+type instance GCD (Succ d) Zero (Succ n) = GCD (Succ Zero) d n
+
+newtype Pointer n = MkPointer Int
+newtype Offset  n = MkOffset Int
+
+-- Usage: multiple 4 :: Offset Three
+multiple :: forall n. (Nat n) => Int -> Offset n
+multiple i = MkOffset (i * toInt (undefined :: n))
+
+-- Usage: add (undefined :: Pointer Six) (undefined :: Offset Four)
+add :: Pointer m -> Offset n -> Pointer (GCD Zero m n)
+add (MkPointer x) (MkOffset y) = MkPointer (x + y)
