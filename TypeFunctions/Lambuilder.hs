6559493dcc227f544a192e6387448bf7a9b1c177
Zac Slade <krakrjak@gmail.com>
2015-04-23 04:58:17 -0500

    Add composition...  cause

--- TypeFunctions/Lambuilder.hs
+++ TypeFunctions/Lambuilder.hs
@@ -5,11 +5,15 @@
 module LamBuilder() where
 
 data LamExpr a where
-	Lit :: Int -> LamExpr Int
-	Add :: LamExpr Int -> LamExpr Int -> LamExpr Int
-	App :: LamExpr (a -> b) -> LamExpr a -> LamExpr b
+	Lit  :: Int -> LamExpr Int
+	Add  :: LamExpr Int -> LamExpr Int -> LamExpr Int
+	Mult :: LamExpr Int -> LamExpr Int -> LamExpr Int
+	App  :: LamExpr (a -> b) -> LamExpr a -> LamExpr b
+	Comp :: LamExpr (b -> c) -> LamExpr (a -> b) -> LamExpr a -> LamExpr c
 
 eval :: LamExpr a -> a
 eval (Lit i) = i
-eval (Add e1 e2) = eval e1 + eval e2
-eval (App e1 e2) = eval e1 (eval e2)
+eval (Add  e1 e2)   = eval e1 + eval e2
+eval (Mult e1 e2)   = eval e1 * eval e2
+eval (App  e1 e2)   = eval e1 $ eval e2
+eval (Comp e1 e2 a) = eval e1 . eval e2 $ eval a
